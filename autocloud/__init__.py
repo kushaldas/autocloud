dc6476a9fbd1686a27df0dc75b814ad9f60ead5b
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-07-17 12:05:32 +0530

    add redis and sqlalachemy configuration

--- autocloud/__init__.py
+++ autocloud/__init__.py
@@ -7,3 +7,6 @@ config.read('/etc/autocloud/autocloud.cfg')
 
 KOJI_SERVER_URL = config.get('autocloud', 'koji_server_url')
 BASE_KOJI_TASK_URL = config.get('autocloud', 'base_koji_task_url')
+REDIS_CONFIG_FILEPATH = config.get('autocloud', 'redis_config_filepath')
+
+SQLALCHEMY_URI = config.get('sqlalchemy', 'uri')
