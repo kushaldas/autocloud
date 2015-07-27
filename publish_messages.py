49708470f0bbf52a94dfc96b0dca8f286da44b61
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-07-27 23:38:12 +0530

    push message to local fedmsg to test the system

---
+++ publish_messages.py
@@ -0,0 +1,13 @@
+import random
+import json
+import fedmsg
+import time
+
+with open('fixtures.json', 'r') as infile:
+    raw_messages = json.load(infile)
+    for raw_message in raw_messages:
+        number = random.randint(1, 11)
+        time.sleep(number)
+        fedmsg.publish(msg=raw_message['msg'],
+                       topic='buildsys.task.state.change')
+
