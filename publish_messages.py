fc1a214ba33a9884c63aa0e05b4896d73eba71ab
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-08-04 14:05:58 +0530

    add sleep in publish messages

--- publish_messages.py
+++ publish_messages.py
@@ -7,6 +7,7 @@ with open('fixtures.json', 'r') as infile:
     raw_messages = json.load(infile)
     for raw_message in raw_messages:
         number = random.randint(1, 11)
+        time.sleep(number)
         fedmsg.publish(msg=raw_message['msg'],
                        topic='buildsys.task.state.change')
 
