6de0ba796c08df24a9dbf7f45d0951dda006be7b
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-08-18 01:20:47 +0530

    add producer file to push messages to fedmsg

---
+++ autocloud/producer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import fedmsg
+
+def publish_to_fedmsg(topic, image_url, image_name, status, build_id):
+    """ Publish the message to fedmsg with image_url, image_name, status and
+    build_id
+    """
+    fedmsg.publish(topic=topic, modname="autocloud", msg={
+        'image_url': image_url,
+        'image_name': image_name,
+        'status': status,
+        'build_id': build_id,
+    })
