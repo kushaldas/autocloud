c581f0a77dc1a67f4a154f73f66a51a378fee1a3
Kushal Das <kushaldas@gmail.com>
2015-09-01 15:04:21 +0530

    Decides the DEBUG value only from config file.

--- autocloud/__init__.py
+++ autocloud/__init__.py
@@ -3,15 +3,17 @@
 import ConfigParser
 import os
 
-DEBUG = True
+DEBUG = False
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
 
 config = ConfigParser.RawConfigParser()
-if DEBUG:
-    config.read("{PROJECT_ROOT}/config/autocloud.cfg".format(
-        PROJECT_ROOT=PROJECT_ROOT))
-else:
-    config.read('/etc/autocloud/autocloud.cfg')
+name = "{PROJECT_ROOT}/config/autocloud.cfg".format(
+        PROJECT_ROOT=PROJECT_ROOT)
+
+if not os.path.exists(name):
+    name = '/etc/autocloud/autocloud.cfg')
+
+config.read(name)
 
 KOJI_SERVER_URL = config.get('autocloud', 'koji_server_url')
 BASE_KOJI_TASK_URL = config.get('autocloud', 'base_koji_task_url')
