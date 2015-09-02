91a0c5535a0fe5a071f60c5c22fc1d015789ec5a
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2015-09-02 15:42:52 +0530

    Update setup.py with metadata

--- setup.py
+++ setup.py
@@ -18,6 +18,14 @@ setup(
     url='https://github.com/kushaldas/autocloud',
     install_requires=requires,
     packages=['autocloud', 'autocloud.web', 'autocloud.utils'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
     entry_points={
         'moksha.consumer': [
             "autocloud_consumer = autocloud.consumer:AutoCloudConsumer",
