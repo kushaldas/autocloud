0193a487af40141d57e27bdbf44e47056ca794cf
Kushal Das <kushaldas@gmail.com>
2015-09-23 11:34:32 +0530

    Fixes #7. Removes all jenkins related code.

--- autocloud/__init__.py
+++ autocloud/__init__.py
@@ -17,10 +17,6 @@ config.read(name)
 KOJI_SERVER_URL = config.get('autocloud', 'koji_server_url')
 BASE_KOJI_TASK_URL = config.get('autocloud', 'base_koji_task_url')
 
-JENKINS_BASE_URL = config.get('jenkins', 'baseurl')
-JENKINS_USERNAME = config.get('jenkins', 'username')
-JENKINS_TOKEN = config.get('jenkins', 'token')
-JENKINS_JOB_NAME = config.get('jenkins', 'job_name')
 
 HOST = config.get('autocloud', 'host') or '127.0.0.1'
 PORT = int(config.get('autocloud', 'port')) or 5000
