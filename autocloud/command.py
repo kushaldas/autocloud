9339938042271d1b7aff58454f9fe75125e7a5b4
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-11-09 00:58:00 +0530

    add the requeue option to autocloud

--- autocloud/command.py
+++ autocloud/command.py
@@ -3,10 +3,13 @@
 import argparse
 import ConfigParser
 import datetime
+import koji
 import os
 
+from retask.task import Task
+from retask.queue import Queue
+
 import autocloud
-import koji
 
 from autocloud.models import init_model, JobDetails
 from autocloud.utils import get_image_name, get_image_url
@@ -34,6 +37,36 @@ def get_koji_details(taskid):
         'name': name,
         'release': release,
     }
+
+
+def enqueue_job(job_id):
+    jobqueue = Queue('jobqueue')
+    jobqueue.connect()
+
+    session = init_model()
+    try:
+        data = session.query(JobDetails).get(str(job_id))
+    except:
+        log.error("Matching data for job id %s not found" % job_id)
+        return None
+
+    taskid = int(data.taskid)
+    koji_details = get_koji_details(taskid)
+    image_name = koji_details['name']
+    image_url = koji_details['image_url']
+    release = koji_details['release']
+
+    info = {
+        'buildid': taskid,
+        'image_url': image_url,
+        'name': image_name,
+        'release': release,
+    }
+
+    task = Task(info)
+    jobqueue.enqueue(task)
+    log.info('Enqueue {jd_id} to redis'.format(jd_id=job_id))
+
 
 def abort_job(job_id):
     session = init_model()
@@ -68,10 +101,18 @@ def abort_job(job_id):
                         image_name=image_name, status='aborted',
                         buildid=taskid, job_id=data.id, release=release)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--abort', nargs='*', type=int)
+    parser.add_argument('-e', '--enqueue', nargs='*', type=int)
     args = parser.parse_args()
 
-    for arg in args.abort:
-        abort_job(arg)
+    if args.abort:
+        for arg in args.abort:
+            abort_job(arg)
+
+    if args.enqueue:
+        for arg in args.enqueue:
+            enqueue_job(arg)
+
