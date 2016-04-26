0288d027322ecf6acae3b0d0abdfa2c1a3e3a93f
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-04-26 14:55:55 +0530

    Remove redundant fixtures file and update publish_messages file

--- publish/publish_messages.py
+++ publish/publish_messages.py
@@ -2,10 +2,11 @@ import json
 import fedmsg
 import time
 
-with open('fixtures.json', 'r') as infile:
+with open('publish/fixtures.json', 'r') as infile:
     raw_messages = json.load(infile)
     for raw_message in raw_messages:
         time.sleep(1)
         fedmsg.publish(msg=raw_message['msg'],
                        topic='pungi.compose.status.change')
+        break
 
