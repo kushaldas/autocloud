2a9b8843767963fed13a9bd145aa5835a4e13dce
Vivek Anand <vivekanand1101@gmail.com>
2017-08-11 19:06:57 +0530

    config: Fix loading of default config values
    If RawConfigParser is not able to find config
    using get(), it doesn't return None, instead
    it raises an Exception.
    
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- autocloud/__init__.py
+++ autocloud/__init__.py
@@ -9,15 +9,16 @@ name = '/etc/autocloud/autocloud.cfg'
 if not os.path.exists(name):
     raise Exception('Please add a proper config file under /etc/autocloud/')
 
-config = ConfigParser.RawConfigParser()
+default = {'host': '127.0.0.1', 'port': 5000}
+config = ConfigParser.RawConfigParser(default)
 config.read(name)
 
 KOJI_SERVER_URL = config.get('autocloud', 'koji_server_url')
 BASE_KOJI_TASK_URL = config.get('autocloud', 'base_koji_task_url')
 
 
-HOST = config.get('autocloud', 'host') or '127.0.0.1'
-PORT = int(config.get('autocloud', 'port')) or 5000
+HOST = config.get('autocloud', 'host')
+PORT = config.getint('autocloud', 'port')
 DEBUG = config.getboolean('autocloud', 'debug')
 
 SQLALCHEMY_URI = config.get('sqlalchemy', 'uri')
